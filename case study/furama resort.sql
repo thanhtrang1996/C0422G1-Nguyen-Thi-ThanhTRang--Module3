36436a6e4fdda2ccdef6f359054a923e60c1f77e
Thanh Trang <you@example.com>
2022-07-17 19:47:11 +0700

    bt

--- case study/furama resort.sql
+++ case study/furama resort.sql
@@ -156,7 +156,7 @@ values ('Nguyễn Văn An','1970-11-07','456231786',10000000,'0901234121','anngu
 ('Nguyễn Công Đạo','1994-01-08','755434343',8000000	,'0988767111','	nguyencongdao12@gmail.com','6 Hoà Khánh, Đồng Nai',2,3,2);
 
 insert into loai_khach(ten_loai_khach)
-value('Diamond'),('Platinium'),('Gold'),('Siler'),('Member');	
+value('Diamond'),('Platinium'),('Gold'),('Silver'),('Member');	
 
 
 insert into khach_hang(ho_ten,ngay_sinh,gioi_tinh,so_cmnd,so_dien_thoai,email,dia_chi,ma_loai_khach)
@@ -164,11 +164,10 @@ values ('Nguyễn Thị Hào','1970-11-07',0,'643431213','0945423362','thihao07@
 ('Phạm Xuân Diệu','1992-08-08',1,'865342123','0954333333','xuandieu92@gmail.com','K77/22 Thái Phiên, Quảng Trị',3),
 ('Trương Đình Nghệ','1990-02-27',1,'488645199','0373213122','nghenhan2702@gmail.com	','K323/12 Ông Ích Khiêm, Vinh',1),
 ('Dương Văn Quan','1981-07-08',1,'543432111','0490039241','duongquan@gmail.com','K453/12 Lê Lợi, Đà Nẵng',1),
-('Dương Văn Quan','1981-07-08',1,'543432111','0490039241','duongquan@gmail.com','K453/12 Lê Lợi, Đà Nẵng',1),
 ('Hoàng Trần Nhi Nhi','199-12-09',0,'795453345','0312345678','nhinhi123@gmail.com','224 Lý Thái Tổ, Gia Lai',4),
 ('Tôn Nữ Mộc Châu','2005-12-06',0,'732434215','0988888844','tonnuchau@gmail.com','37 Yên Thế, Đà Nẵng',4),
 ('Nguyễn Mỹ Kim','1984-04-08',0,'856453123','0912345698','kimcuong84@gmail.com','K123/45 Lê Lợi, Hồ Chí Minh',1),
-('	Nguyễn Thị Hào','1999-04-08',0,'965656433','0763212345','haohao99@gmail.com','55 Nguyễn Văn Linh, Kon Tum',3),
+('Nguyễn Thị Hào','1999-04-08',0,'965656433','0763212345','haohao99@gmail.com','55 Nguyễn Văn Linh, Kon Tum',3),
 ('Trần Đại Danh','1994-07-01',1,'432341235','0643343433','danhhai99@gmail.com','24 Lý Thường Kiệt, Quảng Ngãi',1),
 ('Nguyễn Tâm Đắc','1989-07-01',1,'344343432','0987654321','dactam@gmail.com','22 Ngô Quyền, Đà Nẵng',2);
 
@@ -181,8 +180,8 @@ value ('Villa'),('House'),('Room');
 	
 insert into dich_vu(ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,dich_vu_mien_phi_di_kem,ma_kieu_thue,ma_loai_dich_vu)
 values ('Villa Beach Front',25000,1000000,10,'vip','Có hồ bơi',500,4,null,3,1),
-('House Princess 01',14000,5000000,7,'vip','Có thêm bếp nướng',	null,3,	null,2,2),
-('Room Twin 01',5000,1000000,2,'normal','Có tivi',null,	null,'1 Xe máy, 1 Xe đạp',4,3),
+('House Princess 01',14000,5000000,7,'vip','Có thêm bếp nướng',	null,3,null,2,2),
+('Room Twin 01',5000,1000000,2,'normal','Có tivi',null,null,'1 Xe máy,1 Xe đạp',4,3),
 ('Villa No Beach Front',22000,9000000,8,'normal','Có hồ bơi',300,3,null,3,1),
 ('House Princess 02',10000,	4000000,5,'	normal','Có thêm bếp nướng',null,2,	null,3,2),
 ('Room Twin 02',3000,900000,2,'normal','Có tivi',null,null,'1 Xe máy',4,3);
@@ -199,7 +198,7 @@ values('Karaoke',10000,'giờ','tiện nghi,hiện đại'),
 
 insert into hop_dong(ngay_lam_hop_dong, ngay_ket_thuc,tien_dat_coc,ma_nhan_vien,ma_khach_hang,ma_dich_vu)
 values('2020-12-08','2020-12-08',0,3,1,3),
-('2020-07-14','2020-07-21',	200000,7,3,1),
+('2020-07-14','2020-07-21',200000,7,3,1),
 ('2021-03-15','2021-03-17',50000,3,4,2),
 ('2021-01-14','2021-01-18',100000,7,5,5),
 ('2021-07-14','2021-07-15',0,7,2,6),
@@ -213,6 +212,142 @@ values('2020-12-08','2020-12-08',0,3,1,3),
 
 insert into hop_dong_chi_tiet(so_luong,ma_hop_dong,ma_dich_vu_di_kem)
 value(5,2,4),(8,2,5),(15,2,6),(1,3,1),(11,3,2),(1,1,3),(2,1,2),(2,12,2);
+
+/*2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.*/
+
+select *from nhan_vien
+where (nhan_vien.ho_ten regexp '^[HKT]')  and (char_length(nhan_vien.ho_ten) <=15);
+
+/*3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.*/
+
+select *from khach_hang
+where  (khach_hang.dia_chi like "% Quảng Trị" or khach_hang.dia_chi like "% Đà Nẵng")and ((year(now()) - year(ngay_sinh) between 18 and 50));
+
+/*4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng.
+ Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.*/
+ 
+ 
+select khach_hang.ma_khach_hang,khach_hang.ho_ten,count(hop_dong.ma_khach_hang) so_lan_dat_phong
+from khach_hang
+join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+where loai_khach.ten_loai_khach ='Diamond'
+group by khach_hang.ma_khach_hang
+order by so_lan_dat_phong;
+
+/*5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc,
+ tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem,
+ hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. 
+(những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).*/
+
+
+select khach_hang.ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach,hop_dong.ma_hop_dong,dich_vu.ten_dich_vu,hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc,
+sum((ifnull(hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia,0))+chi_phi_thue ) tong_tien
+from khach_hang
+left  join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+left join hop_dong on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+group by hop_dong.ma_hop_dong
+order by khach_hang.ma_khach_hang;
+
+/*
+6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).*/
+
+
+
+select dich_vu.ma_dich_vu,dich_vu.ten_dich_vu,dich_vu.dien_tich,dich_vu.chi_phi_thue,loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where dich_vu.ma_dich_vu not in(select dich_vu.ma_dich_vu
+from dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where  (( month(hop_dong.ngay_lam_hop_dong) between 1 and 3 ) and  year(hop_dong.ngay_lam_hop_dong) = "2021")
+group by dich_vu.ten_dich_vu
+)
+group by dich_vu.ten_dich_vu
+order by dich_vu.dien_tich desc;
+
+/*7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch 
+vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.*/
+
+
+select dich_vu.ma_dich_vu,dich_vu.ten_dich_vu,dich_vu.dien_tich,dich_vu.so_nguoi_toi_da,
+dich_vu.chi_phi_thue,loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where dich_vu.ma_dich_vu 
+ not in(select dich_vu.ma_dich_vu
+from dich_vu
+join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where (hop_dong.ngay_lam_hop_dong ) between '2021-01-01' and '2021-12-31'
+group by dich_vu.ten_dich_vu) 
+and  year(hop_dong.ngay_lam_hop_dong ) = '2020'
+group by dich_vu.ten_dich_vu;
+
+ /*8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.*/
+
+ /*cách1 */
+
+select khach_hang.ho_ten
+from khach_hang
+group by khach_hang.ho_ten >1;
+
+
+/*cach2*/
+select khach_hang.ho_ten,count(khach_hang.ho_ten) so_nguoi_trung_ten
+from khach_hang
+group by khach_hang.ho_ten
+having so_nguoi_trung_ten >1;
+
+/*cach 3*/
+select distinct  khach_hang.ho_ten
+from khach_hang
+group by khach_hang.ho_ten>1;
+
+/*9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.*/
+
+select month(hop_dong.ngay_lam_hop_dong) thang, count(hop_dong.ngay_lam_hop_dong) so_luong_khach_hang
+from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) = '2021'
+group by month(hop_dong.ngay_lam_hop_dong)
+order by thang;
+
+/*10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, 
+ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).*/
+
+select  hop_dong.ma_hop_dong,hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc,hop_dong.tien_dat_coc,sum(ifnull(hop_dong_chi_tiet.so_luong,0)) so_luong_dich_vu_di_kem
+from hop_dong
+left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+group by hop_dong.ma_hop_dong;
+
+
+
+
+
+ 
+ 
+
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
