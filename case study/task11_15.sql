3665f2db96828b455d5d3244f0ed0f3ebf4545ba
Thanh Trang <you@example.com>
2022-07-18 21:02:12 +0700

    bt

--- case study/task11_15.sql
+++ case study/task11_15.sql
@@ -102,13 +102,24 @@ order by hop_dong.ma_hop_dong;
 
 -- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan,
 -- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
-      
-select nhan_vien.ma_nhan_vien,nhan_vien.ho_ten,trinh_do.ten_trinh_do,bo_phan.ten_bo_phan,nhan_vien.so_dien_thoai,
-nhan_vien.dia_chi
-from nhan_vien
- join trinh_do on nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do
- join bo_phan on nhan_vien.ma_bo_phan = bo_phan.ma_bo_phan
- join hop_dong on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
-where year(hop_dong.ngay_lam_hop_dong) between '2020' and '2021'
-group by hop_dong.ngay_lam_hop_dong >=3;
+
+SELECT 
+    nhan_vien.ma_nhan_vien,
+    nhan_vien.ho_ten,
+    trinh_do.ten_trinh_do,
+    bo_phan.ten_bo_phan,
+    nhan_vien.so_dien_thoai,
+    nhan_vien.dia_chi
+FROM
+    nhan_vien
+        JOIN
+    trinh_do ON nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do
+        JOIN
+    bo_phan ON nhan_vien.ma_bo_phan = bo_phan.ma_bo_phan
+        JOIN
+    hop_dong ON nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+WHERE
+    YEAR(hop_dong.ngay_lam_hop_dong) BETWEEN '2020' AND '2021'
+GROUP BY hop_dong.ma_nhan_vien
+HAVING COUNT(hop_dong.ma_nhan_vien) <= 3;
 
